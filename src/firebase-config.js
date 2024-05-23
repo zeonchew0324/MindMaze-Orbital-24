0ab8aa5dbf6fa8a39892bf001e268ce79c55dc27
Jean Yang <jeanyang.chen@gmail.com>
2024-05-23 09:34:25 +0800

    Firebase auth basic signup function tested via postman

---
+++ src/firebase-config.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID, 
+  MEASUREMENT_ID
+} from '../process.env';
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
