9e12e5622b12f61a41362039e5a585ad595a2169
Chew Tsze Honn <zeonchew0324@gmail.com>
2024-06-04 21:32:15 +0800

    installed tailwind css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  //not sure if tsx can be added
+  content: ["./src/**/*.{html,js,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
